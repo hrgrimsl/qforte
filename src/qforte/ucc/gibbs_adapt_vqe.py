f21dbee441ca59fac1ff5e8b3ae0cec1129cc1a6
Harper Grimsley <hrgrimsl@Harpers-MacBook-Air-2.local>
2024-10-17 16:22:28 -0400

    fixing

--- src/qforte/ucc/gibbs_adapt_vqe.py
+++ src/qforte/ucc/gibbs_adapt_vqe.py
@@ -104,7 +104,7 @@ class Gibbs_ADAPT(UCCVQE):
             self._tamps = res.x
             self.dm_update()
 
-            if abs(self.F - prev_res) < 1e-10:
+            if abs(self.F - prev_res) < 1e-16:
                 return res.x
             else:
                 print(self.F - prev_res, flush = True)
