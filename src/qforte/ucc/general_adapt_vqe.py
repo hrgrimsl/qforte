e4bc264fbc48dba57d052c733995a40d954e850e
Harper Grimsley <hrgrimsl@Harpers-MacBook-Air-2.local>
2025-07-22 09:38:42 -0400

    reformatted

--- src/qforte/ucc/general_adapt_vqe.py
+++ src/qforte/ucc/general_adapt_vqe.py
@@ -24,7 +24,7 @@ class General_ADAPT(UCCVQE):
         verbose=True,
         coupling=None,
         T=0,
-        pool_ref = None
+        pool_ref=None,
     ):
         """
         algorithm, string: Choices are adapt-vqe, more-adapt-vqe, and hot-adapt-vqe
